433b3e84bb756016a79cfa65544d8731f6e74108
Louie Young <louie.young@katalyst.com.au>
2022-02-22 14:57:03 +1030

    Added validation and relations to micropost and user models

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,7 @@
 class Micropost < ApplicationRecord
-  validates :content, length: { maximum: 140 }
-
   belongs_to :user
+
+  validates :content,
+            length: { maximum: 140 },
+            presence: true
 end
